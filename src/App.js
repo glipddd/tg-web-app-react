2cbe8269922fee750d3b3fa790cd2a2954e79a90
Glip <test@test.ru>
2022-11-30 00:47:02 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { useEffect } from 'react'
 import './App.css'
+import { useTelegram } from './hooks/useTelegram'
 
 function App() {
+	const { tg, onToggleButton } = useTelegram()
+
 	useEffect(() => {
 		tg.ready()
 	}, [])
 
-	return <div className='App'>work</div>
+	return (
+		<div className='App'>
+			<button onClick={onToggleButton}>toggle</button>
+		</div>
+	)
 }
 export default App
