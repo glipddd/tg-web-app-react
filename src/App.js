1978bb5bdc7cfc851e8fb85cb1af390ba721ae73
Glip <test@test.ru>
2022-11-30 01:10:36 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
 import { useTelegram } from './hooks/useTelegram'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 
 	return (
 		<div className='App'>
+			<Header />
 			<button onClick={onToggleButton}>toggle</button>
 		</div>
 	)
