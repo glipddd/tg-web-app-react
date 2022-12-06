3af876805bac66f66fd93d1494d1d8c13cdcf9f3
Glip <test@test.ru>
2022-12-06 05:33:05 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Form from './components/Form/Form'
 import Header from './components/Header/Header'
+import ProductList from './components/ProductList/ProductList'
 import { useTelegram } from './hooks/useTelegram'
 
 function App() {
@@ -13,7 +16,10 @@ function App() {
 	return (
 		<div className='App'>
 			<Header />
-			<button onClick={onToggleButton}>toggle</button>
+			<Routes>
+				<Route index element={<ProductList />} />
+				<Route path={'form'} element={<Form />} />
+			</Routes>
 		</div>
 	)
 }
